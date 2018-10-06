a00dd3c09a13ec475f5bb32dc2887617faa8e9ae
visanu <zeero_725@hotmail.com>
2018-10-06 18:16:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 			
 			$messages = [
 			 'type' => 'text',
-			 'text' => $text
+			 'text' => "sssf sdfs กฟหดหฟ"
 			];
 			$reply["to"] = "U1355bc358b90258582531ecb6172dc95"; 
 			$reply["messages"][0] = $messages;
