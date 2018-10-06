cf6419d5d21e13990e2f7185f148100037b0a4ce
visanu <zeero_725@hotmail.com>
2018-10-06 18:00:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,7 +12,8 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			$text =  $event['message']['text'];
-			$qry_str = "?data=".rawurlencode($text)."&event=".$content;
+			$text = str_replace ( ' ', '%20', $text );
+			$qry_str = "?data=".urlencode($text)."&event=".$content;
 			$ch = curl_init();
 
 			// Set query data here with the URL
