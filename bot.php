2c7084eb392cfb53a3ff0c2357aac95e45030245
visanu <zeero_725@hotmail.com>
2018-10-06 19:05:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,6 +13,9 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			$text =  $event['message']['text'];
 			$qry_str = "?data=".urlencode($text)."&event=".$content;
+			
+			
+			
 			$ch = curl_init();
 
 			// Set query data here with the URL
@@ -22,6 +25,27 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_TIMEOUT, 3);
 			$content = trim(curl_exec($ch));
 			curl_close($ch);
+			
+			
+			$qry_str = "data=".urlencode($text)."&event=".$content;
+			$ch = curl_init();
+		
+			curl_setopt($ch, CURLOPT_URL,"http://notify.moomnee.com/line_bot/echo_message.php");
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS,$qry_str);
+
+			// In real life you should use something like:
+			// curl_setopt($ch, CURLOPT_POSTFIELDS, 
+			//          http_build_query(array('postvar1' => 'value1')));
+
+			// Receive server response ...
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+			$server_output = curl_exec($ch);
+
+			curl_close ($ch);
+			
+			
 		}
 	}
 	
