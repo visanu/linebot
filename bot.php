3b93028ab8ed9e93a762a90179a2d29227e603c4
visanu <zeero_725@hotmail.com>
2018-10-06 18:27:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			$text =  $event['message']['text'];
-			$qry_str = "?data=".($text)."&event=".$content;
+			$qry_str = "?data=".urlencode($text)."&event=".$content;
 			$ch = curl_init();
 
 			// Set query data here with the URL
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 			
 			$messages = [
 			 'type' => 'text',
-			 'text' => $text
+			 'text' => $text."1"
 			];
 			$line_msg["to"] = "U1355bc358b90258582531ecb6172dc95"; 
 			$line_msg["messages"][0] = $messages;
