cc9a4fe648cb05fe3ba41b581bed697eb974cf48
visanu <zeero_725@hotmail.com>
2018-10-06 17:35:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -25,29 +25,7 @@ if (!is_null($events['events'])) {
 
 		}
 	}
-	////////////////
-	$messages = [
-	 'type' => 'text',
-	 'text' => urlencode($text)
-	];
-	$reply["to"] = "U1355bc358b90258582531ecb6172dc95"; 
-	$reply["messages"][0] = $messages;
-	putMessageLine($reply);
-	function putMessageLine($line_msg){
-	    $ch = curl_init( 'https://api.line.me/v2/bot/message/push' );
-	    # Setup request to send json via POST.
-	    $authorization = "Authorization: Bearer PmDddl8WuDAkEfXMn31RRp51mGKnckP1eQ/FicaegAdtDn8+6lqTl/+X3wp1yuYUSGGFw4AcFM64SqRmLBOERVxDJUh3EEGNZb2lzLvGIwyhaJOpyiDwZijKDdiotKRGK7Chf5QR+F3+v4wPgocg6wdB04t89/1O/w1cDnyilFU=";
-	    $payload = json_encode( $line_msg );
-	    //print_r($payload);
-	    curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
-	    curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json',$authorization));
-	    # Return response instead of printing.
-	    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-	    # Send request.
-	    $result = curl_exec($ch);
-	    curl_close($ch);
-	    print_r($result);
-	}
+	
 }
 echo "OK";
 ?>
